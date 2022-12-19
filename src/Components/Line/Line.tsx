f8913f1ddc944a957f5b0efc1535b3ded16dd039
Jarvismkennedy <77736532+Jarvismkennedy@users.noreply.github.com>
2022-12-19 15:21:00 -0500

    making plan

--- src/Components/Line/Line.tsx
+++ src/Components/Line/Line.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import { Series } from './types';
 
+// This is going to be canvas Line.
+// to do: everything... implement canvas rendering methods for every layer.
+// should have ability to pass a canvas ref to Line rather than Line generating its own ref
+// so that its easier to print to pdf.
+// use d-3 to generate the shapes.
+// then think of how to do this in 3 dimensions. maybe need to implement my own render methods if d3 doesnt have any.
+//
+
 export interface LineProps {
     data: Array<Series>;
 }
